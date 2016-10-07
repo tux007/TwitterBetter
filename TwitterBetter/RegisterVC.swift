89ef5d7eaa0e859da03cdeb6e48d11866c9b34c7
Steimann Thomas <thomas.steimann@resolvex.ch>
2016-10-07 03:53:25 +0200

    Registering users with app (backend is ready)

--- TwitterBetter/RegisterVC.swift
+++ TwitterBetter/RegisterVC.swift
@@ -42,9 +42,72 @@ class RegisterVC: UIViewController {
             
             // create user in mySQL DB
             
+            // url to php file
+            let url = NSURL(string: "http://localhost/TwitterBetter/register.php")!
+            
+            // request to this file
+            var request = URLRequest(url: url as URL)
+            
+            // method to pass data to this file (e.g. via POST)
+            request.httpMethod = "POST"
+            
+            // body to be appended to url
+            let body = "username=\(usernameTxt.text!.lowercased())&password=\(passwordTxt.text!)&email=\(emailTxt.text!)&fullname=\(firstnameTxt.text!)%20\(lastnameTxt.text!)"
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            
+            // proceed request
+            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                
+                if error == nil {
+                    
+                    // get main queue in code process to communicate back to UI
+                    DispatchQueue.main.async {
+                        
+                        do {
+                            // get json result
+                            let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                            
+                            // assign json to new var parseJSON in guard/secured way
+                            guard let parseJSON = json else {
+                                print("Error while parsing")
+                                return
+                            }
+                            
+                            // get id from parseJSON dictionary
+                            let id = parseJSON["id"]
+                            
+                            // successfully registered
+                            if id != nil {
+                                
+                                print(parseJSON)
+                               
+                                
+                            }
+                            
+                        } catch {
+                            
+                            print("Caught an error: \(error)")
+                            
+                        }
+                        
+                    }
+                    
+                    // if unable to proceed request
+                } else {
+                    
+                    print("error: \(error)")
+                        
+                    }
+                
+                // launch prepared session
+            }).resume()
             
         }
+        
+        
     }
     
 }
+
+
 
